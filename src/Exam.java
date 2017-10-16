368906d0b2944ce49010aabbfd9191b92b7babbc
SteRoa <steroagna66@gmail.com>
2017-10-16 22:13:06 +0200

    exam class

--- src/Exam.java
+++ src/Exam.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Exam {
+    private int studentsEnrolled;
+
+    public Exam(int studentsEnrolled) {
+        this.studentsEnrolled = studentsEnrolled;
+    }
+
+    public int getStudentsEnrolled() {
+
+        return studentsEnrolled;
+    }
 }
