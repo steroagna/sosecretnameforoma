e55dc57e6a6b4fe6274a9109b8887795205ff702
SteRoa <steroagna66@gmail.com>
2017-10-17 17:30:06 +0200

    scanner funzionante. Da finire la logica. inserita le istance01 di prova

--- src/Exam.java
+++ src/Exam.java
@@ -1,12 +1,17 @@
 public class Exam {
+    private int id;
     private int studentsEnrolled;
 
-    public Exam(int studentsEnrolled) {
+    public Exam(int id, int studentsEnrolled) {
+        this.id = id;
         this.studentsEnrolled = studentsEnrolled;
     }
 
-    public int getStudentsEnrolled() {
+    public int getId() {
+        return id;
+    }
 
+    public int getStudentsEnrolled() {
         return studentsEnrolled;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,16 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+    /**
+     * Slot value
+     */
+    static int examsNumber = 0;
 
     /**
      * Slot value
@@ -20,12 +27,12 @@ public class Main {
     /**
      * Matrix of conflicts (STUD/EXAM)
      */
-    static int[][] conflicts;
+    static List<Integer> conflicts = new ArrayList<>();
 
     /**
-     * Matrix of conflicts (EXAM/EXAM)
+     * Matrix of conflicts (STUD/EXAM)
      */
-    static int[][] examsGraph;
+    static HashMap<Integer, Exam> Exams = new HashMap<>();
 
     /**
      * Objective function value
@@ -36,26 +43,17 @@ public class Main {
     public static void main(String[] args) {
 
         long startTime = System.currentTimeMillis(), elapsedTime;
-        FileWriter writer = null;
-
         try {
-            readInputFiles(args[1]);
+            readInputFiles(args[0]);
+            elapsedTime = (System.currentTimeMillis() - startTime);
+            writeOutput(elapsedTime);
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-
         } catch (IOException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
         }
-
-        elapsedTime = (System.currentTimeMillis() - startTime);
-        try {
-			writeOutput(writer, elapsedTime);
-			writer.close();
-        } catch (IOException e) {
-			e.printStackTrace();
-		}
     }
 
     @SuppressWarnings("unused")
@@ -67,21 +65,45 @@ public class Main {
 
         Scanner scanner  = new Scanner(slotsFile);
         slotsNumber = scanner.nextInt();
-
-        scanner  = new Scanner(studentsFile);
-        while (scanner.hasNextLine()) {
-            int stud = scanner.nextInt();
-            int exam = scanner.nextInt();
-            conflicts[stud][exam] = 1;
-        }
+        scanner.close();
 
         scanner  = new Scanner(examsFile);
+        while (scanner.hasNextLine() && scanner.hasNext()) {
+            int examId = scanner.nextInt();
+            int numberOfStudents = scanner.nextInt();
+            Exam exam = new Exam(examId, numberOfStudents);
+            Exams.put(examId, exam);
+            examsNumber++;
+        }
+        scanner.close();
+        System.out.println(examsNumber);
+        /**
+         * Matrix of conflicts (EXAM/EXAM)
+         */
+        int[][] examsGraph = new int[examsNumber][examsNumber];
 
+        scanner  = new Scanner(studentsFile);
+        String  stud, actualStud = null;
+        int exam = 0;
+        while (scanner.hasNextLine() && scanner.hasNext()) {
+            stud = scanner.next();
+            if (actualStud != null && !conflicts.isEmpty()) {
+                if (!actualStud.equals(stud)) {
+                    System.out.println(exam);
+                    examsGraph[exam-1][exam-1] = 0;
+                }
+            }
+            actualStud = stud;
+            exam = scanner.nextInt();
+            conflicts.add(exam);
+        }
+        scanner.close();
+        return;
     }
 
-    private static void writeOutput(FileWriter writer, long time) {
+    private static void writeOutput(long time) {
 
-        System.out.println(time);
+        System.out.println("Elaborazione dati in " + time + " millisec");
 
     }
 }
